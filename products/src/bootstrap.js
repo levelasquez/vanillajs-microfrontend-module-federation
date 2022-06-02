35908975a9c3f7a866e4be95bdcb10c1878aad0a
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-02 12:36:50 -0400

    sharing dependencies between apps

---
+++ products/src/bootstrap.js
@@ -0,0 +1,22 @@
+import faker from 'faker'
+
+const mount = (element) => {
+  let products = ''
+
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName()
+    products += `<div>${name}</div>`
+  }
+
+  element.innerHTML = products
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const element = document.querySelector('#dev-products')
+
+  if (element) {
+    mount(element)
+  }
+}
+
+export { mount }
